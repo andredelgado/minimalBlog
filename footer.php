9300efa8f25d11d44879ac7d4c2dde27350691d7
andredelgado <andrevieiradelgado@gmail.com>
2014-03-23 23:30:11 +0000

    add footer links

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
-<footer>
-    <div class="footerCredits"><section class="colophon" role="contentinfo">
-  <small class="copyright half left">
-    © 2013
-  </small><!-- .copyright -->
-  <small class="credits half right">
-     Proudly powered by <a href="http://wordpress.org/">WordPress</a>.
-  </small><!-- .credits -->
-</section></div>
-  </footer>
+    <footer>
+            <section role="contentinfo">
+      <small>
+        © 2014
+      </small>
+      <small>
+         <a href="#"><?php bloginfo('name') ?></a> Proudly powered by <a href="http://andreny.com/minimalBlog">minimalBlog Theme</a> and <a href="http://wordpress.com">Wordpress</a>.
+      </small>
+    </section>
+      </footer>
 
-      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <!--<script src="js/bootstrap.min.js"></script>-->
-  <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+          <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <!--<script src="js/bootstrap.min.js"></script>-->
+      <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
