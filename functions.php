314b619b03eca1efa5204386f63ab77ebdde0612
andredelgado <andrevieiradelgado@gmail.com>
2014-03-24 19:40:56 +0000

    add widget area

--- functions.php
+++ functions.php
@@ -8,8 +8,21 @@ function minimalBlog_theme_support() {
     add_theme_support( 'post-thumbnails' );
 }
 
+function minimalBlog_widgets_init() {
+	register_sidebar( array(
+		'name' => 'Footer Widget Area',
+		'id' => 'footer-widget-area',
+		'description' => 'Widget area located on footer',
+		'before_widget' => '<div class="widgetBox>',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widgetTitle">',
+		'after_title' => '</h3>'
+	));
+}
+
 add_action('init', 'minimalBlog_register_theme_menu');
 
 add_action( 'after_setup_theme', 'minimalBlog_theme_support' );
 
+add_action('widgets_init', 'minimalBlog_widgets_init');
 ?>
