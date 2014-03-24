8cf3b7a320e54964c6a0ed0e31f1ae0aa7191cfd
andredelgado <andrevieiradelgado@gmail.com>
2014-03-24 19:56:15 +0000

    add widget area

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,8 @@
 
     <div class="footerBar">
+    	<?php if ( is_active_sidebar( 'footer-widget-area' ) ) { ?>
+  <aside class="sidebar widget-area one-third right" role="complementary">
+    <?php dynamic_sidebar( 'footer-widget-area' ); ?>
+  </aside>
+<?php } ?>
     </div>
