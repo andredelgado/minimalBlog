9b96d0d7720271380808e7546d0c396368557c10
andredelgado <andrevieiradelgado@gmail.com>
2014-03-24 16:55:39 +0000

    add Featured function

--- functions.php
+++ functions.php
@@ -4,6 +4,12 @@ function minimalBlog_register_theme_menu() {
 	register_nav_menu('primary', 'main menu');	
 }
 
+function minimalBlog_theme_support() {
+    add_theme_support( 'post-thumbnails' );
+}
+
 add_action('init', 'minimalBlog_register_theme_menu');
+
+add_action( 'after_setup_theme', 'minimalBlog_theme_support' );
 
 ?>
