7930022b86d02ef90ae9d20abb84487429541a31
andredelgado <andrevieiradelgado@gmail.com>
2014-03-24 17:00:31 +0000

    add featured

--- single.php
+++ single.php
@@ -1,8 +1,16 @@
 <?php get_header(); ?>
 
+
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="featured">
-    
+    <?php if ( has_post_thumbnail() ) { ?>
+    <a href="<?php the_permalink(); ?>">
+    <?php the_post_thumbnail( 'medium', array( 'class' => 'left',
+            'alt'   => trim( strip_tags( $wp_postmeta->_wp_attachment_image_alt ) )
+        ) ); ?>
+    </a>
+    <?php } ?>
   </div>
   <section class="article">
     <div class="container">
